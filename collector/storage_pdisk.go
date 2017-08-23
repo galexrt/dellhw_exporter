1823932a1800d7d8ae75cba583b0ae2816cd9327
Alexander Trost <galexrt@googlemail.com>
2017-08-23 20:11:48 +0200

    collector: Added storage_pdisk collector

---
+++ collector/storage_pdisk.go
@@ -0,0 +1,46 @@
+package collector
+
+import (
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type storagePdiskCollector struct {
+	current []*prometheus.Desc
+}
+
+func init() {
+	Factories["storage_pdisk"] = NewStoragePdiskCollector
+}
+
+func NewStoragePdiskCollector() (Collector, error) {
+	return &storagePdiskCollector{}, nil
+}
+
+func (c *storagePdiskCollector) Update(ch chan<- prometheus.Metric) error {
+	controllers, err := or.StorageController()
+	if err != nil {
+		return err
+	}
+	for cid := range controllers {
+		storagePdisk, err := or.StoragePdisk(strconv.Itoa(cid))
+		if err != nil {
+			return err
+		}
+		for _, value := range storagePdisk {
+			float, err := strconv.ParseFloat(value.Value, 64)
+			if err != nil {
+				return err
+			}
+			current := prometheus.NewDesc(
+				prometheus.BuildFQName(Namespace, "", value.Name),
+				"Overall status of physical disks.",
+				nil, value.Labels)
+			ch <- prometheus.MustNewConstMetric(
+				current, prometheus.GaugeValue, float)
+		}
+	}
+
+	return nil
+}
