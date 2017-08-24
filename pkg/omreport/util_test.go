a36d3d6e057e72e91a94d82dc9740abf8d9ca0b3
Alexander Trost <galexrt@googlemail.com>
2017-08-24 10:10:34 +0200

    pkg/omreport: Added tests for util severity()

---
+++ pkg/omreport/util_test.go
@@ -0,0 +1,35 @@
+package omreport
+
+import (
+	"reflect"
+	"testing"
+)
+
+type SeverityTestResult struct {
+	Input  string
+	Output string
+}
+
+var severityTests = []SeverityTestResult{
+	{
+		Input:  "Ok",
+		Output: "0",
+	},
+	{
+		Input:  "Unknown",
+		Output: "1",
+	},
+	{
+		Input:  "Non-Critical",
+		Output: "0",
+	},
+}
+
+func TestSeverity(t *testing.T) {
+	for _, result := range severityTests {
+		value := severity(result.Input)
+		if !reflect.DeepEqual(value, result.Output) {
+			t.Errorf("severity result not equal: %+v - %+v\n", value, result.Output)
+		}
+	}
+}
